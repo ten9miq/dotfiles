eb10cf3b10551f865143fa0ea8c8a315db6467ca
53JIlLenWe11 <ruin.of.messger@gmail.com>
2019-10-20 21:50:39 +0900

    fihsのlsのaliasが動作しないので修正

--- fish/fish/aliases.fish
+++ fish/fish/aliases.fish
@@ -4,9 +4,9 @@ set LSCOLORS Exfxcxdxbxegedabagacad
 set LS_COLORS 'di=01;96:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
 alias l ls
-alias ls '\ls -AXFhv --group-directories-first --color=auto'
-alias ll '\ls -lXFhv --group-directories-first --color=auto'
-alias la '\ls -alXFhv --group-directories-first --color=auto'
+alias ls 'ls -AXFhv --group-directories-first --color=auto'
+alias ll 'ls -lXFhv --group-directories-first --color=auto'
+alias la 'ls -alXFhv --group-directories-first --color=auto'
 
 alias .. 'cd ..'
 alias ... 'cd ../..'
