8d452b8a1838334180200af12f96cc1bf8becb3c
53JIlLenWe11 <ruin.of.messger@gmail.com>
2019-10-20 21:43:48 +0900

    fishのinit時の.configファイルの生成パスがおかしいので修正

--- fish/init.sh
+++ fish/init.sh
@@ -3,7 +3,7 @@ THIS_SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 # PROJECT_PATHなどのsetup時の環境変数の読み込みを行う
 source $THIS_SCRIPT_PATH/../setup_env.sh
 
-\mkdir -p $THIS_SCRIPT_PATH/.config/
-\cp -r $THIS_SCRIPT_PATH/fish ~/.config/
+\mkdir -p $HOME/.config/
+\cp -r $THIS_SCRIPT_PATH/fish $HOME/.config/
 
 exit $?
