562ee269c0d9a291672ea0096c47e834fc13a678
53JIlLenWe11 <ruin.of.messger@gmail.com>
2020-01-24 16:38:36 +0900

    ssh configのコピーした先をログとして表示するよに修正し､cygwinのときのコピーを変更

--- ssh/init.sh
+++ ssh/init.sh
@@ -7,6 +7,7 @@ if [ `os_type` = 'linux' ] \
   || [ `os_type` = "wsl" ]; then
 
   copy_target="$HOME/.ssh"
+  log_print "ssh config copy to ${copy_target}"
   mkdir -p $copy_target
   # 指定のファイル以外をコピーしてかつ、配下のディレクトリをコピーする
   find $THIS_SCRIPT_PATH -type f -not -name 'init.sh' | while read -r find_path;
@@ -15,10 +16,15 @@ if [ `os_type` = 'linux' ] \
     relative_path=${find_path##$THIS_SCRIPT_PATH/}
     # コピー先のディレクトリがなければ作成する
     mkdir -p $copy_target/$(dirname $relative_path);
-    cp $find_path $copy_target/$relative_path;
+    \cp $find_path $copy_target/$relative_path;
   done
-  chmod 700 $HOME/.ssh
-  chmod 600 $HOME/.ssh/config
+  chmod 700 $copy_target
+  chmod 600 $copy_target/config
+elif [ `os_type` = 'cygwin' ]; then
+  copy_target="$ALLUSERSPROFILE/ssh" # C:\ProgramData/
+  log_print "ssh config copy to ${copy_target}"
+  mkdir -p $copy_target
+  \cp $THIS_SCRIPT_PATH/config $copy_target/ssh_config
 fi
 
 exit $?
