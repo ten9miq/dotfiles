af3c276562301965c1ec2e3485333b3f0854324e
53JIlLenWe11 <ruin.of.messger@gmail.com>
2020-01-28 17:01:14 +0900

    zinitがcloneできない環境下でのスクリプトをコメントアウトして追加

--- zsh/init.sh
+++ zsh/init.sh
@@ -5,6 +5,25 @@ source $THIS_SCRIPT_PATH/../setup_env.sh
 
 \cp $THIS_SCRIPT_PATH/.zshrc ~/
 \cp $THIS_SCRIPT_PATH/.zprofile ~/
-\cp -r $THIS_SCRIPT_PATH/.zsh/ ~/
+\cp -pr $THIS_SCRIPT_PATH/.zsh/ ~/
+
+
+# dotfileの展開先でgithubにアクセスできない場合はzipにしたプラグインのファイルを展開するスクリプト
+# if [ `os_type` = 'linux'  ]; then
+#   zinit_path="$HOME/.zinit/"
+#   mkdir -p $zinit_path/plugins/
+#   tar zxf $THIS_SCRIPT_PATH/zinit.tgz/bin.tgz -C $zinit_path
+# 
+#   find $THIS_SCRIPT_PATH/zinit.tgz/plugins/ -type f -name "*.tgz" | xargs -n 1 -IXXX tar zxf XXX -C $zinit_path/plugins/
+# 
+#   ln -fs $zinit_path/bin/_zinit $HOME/.zinit/plugins/_local---zinit/
+#   if `has zsh` ; then
+#     log_print "zinit creinstall exec"
+#     zsh -ic "zinit creinstall -q $HOME/.zinit"
+#   else
+#     skip_print 'not install zsh. please zsh installed to "zinit creinstall -q $HOME/.zinit" commnad'
+#   fi
+# fi
+
 
 exit $?
