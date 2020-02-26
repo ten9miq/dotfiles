9866c83a86b43bf62c142b47b649661de5d9d936
53JIlLenWe11 <ruin.of.messger@gmail.com>
2020-02-26 15:17:40 +0900

    shorten-pathの処理を可変しやすいように改修

--- shell_common/prompt.bash
+++ shell_common/prompt.bash
@@ -5,11 +5,28 @@ hostname_head3() { echo $HOSTNAME | sed -r 's@(([^\.]*)(\.[^\.]*){1,2}).*$@\1@';
 MYPSDIR_AWK=$(cat << 'EOF'
 BEGIN { FS = OFS = "/" }
 {
+  VIEW_LEFT_PATH_COUNT=3
+  VIEW_RIGHT_PATH_COUNT=5
+
+  shorten_length=50
+  shorten_path_depth=1+VIEW_LEFT_PATH_COUNT+VIEW_RIGHT_PATH_COUNT
+
   sub(ENVIRON["HOME"], "~");
-  if (length($0) > 16 && NF > 6)
-    print $1,$2,".."NF-6"..",$(NF-3),$(NF-2),$(NF-1),$NF
-  else
+  if (length($0) > shorten_length && NF > shorten_path_depth){
+    # パス省略の左側
+    for (j = 1; j <= VIEW_LEFT_PATH_COUNT ; j++ ) {
+      printf "%s/",$j
+    }
+    # 省略部分
+    printf "..%s..",NF-VIEW_RIGHT_PATH_COUNT-VIEW_LEFT_PATH_COUNT-1
+    # パス省略の右側
+    for (i = VIEW_RIGHT_PATH_COUNT; i >= 0 ; i-- ) {
+      printf "/%s",$(NF-i)
+    }
+    printf "\n"
+  }else{
     print $0
+  }
 }
 EOF
 )
