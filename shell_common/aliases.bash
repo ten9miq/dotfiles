4b4fd88d728ebda66d0a75211dc65044805232e3
53JIlLenWe11 <ruin.of.messger@gmail.com>
2022-08-23 12:41:59 +0900

    vin aliasで検索時のハイライトを有効にする

--- shell_common/aliases.bash
+++ shell_common/aliases.bash
@@ -3,7 +3,7 @@ alias v='vim'
 alias vi='vim'
 # -n スワップファイルなし -u NONE pluginもvimrcも読み込まない -i NONE viminfoを読み込まない
 # noremapで<Right> <Left>が使えないのでキーコードで設定
-alias vin='vim -n -u NONE -i NONE --cmd "set nowrap" --cmd "set number" --cmd "noremap OC zL" --cmd "noremap OD zH"'
+alias vin='vim -n -u NONE -i NONE --cmd "set nowrap" --cmd "set number" --cmd "set hlsearch" --cmd "noremap OC zL" --cmd "noremap OD zH"'
 alias l='\ls'
 alias ls='\ls -AXFhv --group-directories-first --color=auto'
 
