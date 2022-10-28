0f798eeaaff763900ba28e4d261e43f46b1f5759
53JIlLenWe11 <ruin.of.messger@gmail.com>
2022-10-28 13:13:55 +0900

    mysqlのaliasでlessの横スクロールを画面半分にし、vimでの閲覧時に画面分割可能にし同期させる

--- shell_common/aliases.bash
+++ shell_common/aliases.bash
@@ -32,8 +32,10 @@ alias rsync='rsync -ah --no-i-r --info=progress2'
 alias rsyncR='rsync -ah --no-i-r --info=progress2 --checksum --append-verify --partial'
 
 # mysqlのページャを設定して実行するコマンド
-alias mysqll='mysql --pager="less -S -N -i -F -X --shift=30"'
-alias mysqlv='mysql --pager="vim -R - -c \"set ft=sql\" -c \"set nowrap\" -c \"noremap <Right> zL\" -c \"noremap <Left> zH\""'
+# less  --shift=0 水平スクロールが画面半分移動
+alias mysqll='mysql --pager="less -S -N -i -F -X --shift=0"'
+# vim  3<c-w>s 3行で行分割 3<c-w> _ で高さ指定し直し 閉じる時は :qa
+alias mysqlv='mysql --pager="vim -R - -c \"set ft=sql\" -c \"set nowrap\" -c \"noremap <Right> zL\" -c \"noremap <Left> zH\" -c \"set scrollopt=hor\" -c \"set scb\""'# mysqlのページャを設定して実行するコマンド
 
 alias watch='watch -n0.3 '
 waps(){
